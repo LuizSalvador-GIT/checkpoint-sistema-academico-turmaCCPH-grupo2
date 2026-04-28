00d905f6eb6090dd7d575cd6d3d48e89d4f9ccf5
Ricardo Vaca <126781960+RVaca1@users.noreply.github.com>
2026-04-28 11:28:57 -0300

    media adicionada
    Fix the media calculation and output messages based on the average score.

---
+++ media.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float calcularMedia(float nota1, float nota2) {
+    return (nota1 + nota2) / 2.0;
+}
+
+int main() {
+    float nota1, nota2, media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    media = calcularMedia(nota1, nota2);
+
+
+    if (media = 6.0) {
+        printf("Aluno aprovado\n");
+    } else {
+        printf("Aluno reprovado\n");
+    }
+
+    return 0;
+}

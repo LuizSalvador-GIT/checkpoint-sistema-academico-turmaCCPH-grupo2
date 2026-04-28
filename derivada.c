250866dab703640b257a5e6d05a39d4fb6406104
Caio Marinho <rm572873@fiap.com.br>
2026-04-28 10:47:51 -0300

    criacao de interface e menu simples para opcao 5

--- derivada.c
+++ derivada.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(){
-
+    printf("==== DERIVADA DE FUNCAO SIMPLES ====\n\n");
+    printf("Modelo de funcao: f(x) = ax^2 + bx + c");
+    printf("Indique os coeficientes a, b e c da funcao: \n");
+    
 
     return 0;
 }
